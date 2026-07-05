53afa44b8613f72b5aa26d46dd238f12ed069e33
Ariadne Conill <ariadne@ariadne.space>
2026-07-05 15:04:30 -0700

    Refactor shell VFS test phases

--- tools/test-shell.sh
+++ tools/test-shell.sh
@@ -274,21 +274,130 @@ wait_for_fixed "$log" "linux pathmax ok" "linux pathmax regression failed" 75 22
 send_script <<'EOF_USER_SMOKE'
 busybox df / /tmp /proc >/dev/null && echo STATFS_DF_OK
 EOF_USER_SMOKE
-printf 'busybox cat /hello.txt && echo UNION_ROOT_LOWER_OK\nbusybox mv /rename-lower.txt /rename-upper.txt && echo UNION_LOWER_RENAME_CREATE_OK\nbusybox cat /rename-upper.txt | busybox grep "nested rootfs file" && echo UNION_LOWER_RENAME_READ_OK\nbusybox test ! -e /rename-lower.txt && echo UNION_LOWER_RENAME_OLD_GONE_OK\nbusybox test -e /.upper/rename-upper.txt && echo UNION_LOWER_RENAME_UPPER_OK\n' >&3
+send_script <<'EOF_UNION_RENAME'
+busybox cat /hello.txt && echo UNION_ROOT_LOWER_OK
+busybox mv /rename-lower.txt /rename-upper.txt && echo UNION_LOWER_RENAME_CREATE_OK
+busybox cat /rename-upper.txt | busybox grep "nested rootfs file" && echo UNION_LOWER_RENAME_READ_OK
+busybox test ! -e /rename-lower.txt && echo UNION_LOWER_RENAME_OLD_GONE_OK
+busybox test -e /.upper/rename-upper.txt && echo UNION_LOWER_RENAME_UPPER_OK
+EOF_UNION_RENAME
 wait_for_fixed "$log" "UNION_LOWER_RENAME_UPPER_OK" "unionfs lower rename commands did not drain" 45 220
-printf 'busybox ln /hello.txt /hello-hard.txt && echo UNION_LOWER_HARDLINK_CREATE_OK\nbusybox cat /hello-hard.txt | busybox grep "rootfs: module" && echo UNION_LOWER_HARDLINK_READ_OK\nbusybox test -e /.upper/hello.txt && busybox test -e /.upper/hello-hard.txt && echo UNION_LOWER_HARDLINK_COPYUP_OK\necho UNION_ROOT_BASE_PAYLOAD > /created.txt\necho UNION_ROOT_APPEND_PAYLOAD >> /created.txt\nbusybox cat /created.txt && echo UNION_ROOT_APPEND_CAT_OK\nbusybox cat /.upper/created.txt && echo UNION_ROOT_UPPER_BACKING_OK\nUNION_LONG_NAME=bunix-union-root-name-longer-than-sixteen.txt\necho UNION_ROOT_LONG_PAYLOAD > "/$UNION_LONG_NAME"\nbusybox ls / | busybox grep "$UNION_LONG_NAME" && echo UNION_ROOT_LONG_READDIR_OK\nbusybox cat "/$UNION_LONG_NAME" && echo UNION_ROOT_LONG_CAT_OK\nUNION_NAME_MAX=bunix-union-name-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.txt\necho UNION_NAME_MAX_PAYLOAD > "/$UNION_NAME_MAX"\nbusybox ls / | busybox grep "$UNION_NAME_MAX" && echo UNION_NAME_MAX_READDIR_OK\nbusybox cat "/$UNION_NAME_MAX" && echo UNION_NAME_MAX_CAT_OK\nbusybox ls / | busybox grep created.txt && echo UNION_ROOT_READDIR_UPPER_OK\nbusybox ls / | busybox grep secret.txt && echo UNION_ROOT_READDIR_LOWER_OK\nbusybox rm /created.txt && echo UNION_ROOT_UNLINK_UPPER_OK\nbusybox test ! -e /created.txt && echo UNION_ROOT_UNLINK_UPPER_GONE_OK\nbusybox cat /hello.txt && echo UNION_ROOT_LOWER_STILL_OK\n' >&3
+send_script <<'EOF_UNION_ROOT'
+busybox ln /hello.txt /hello-hard.txt && echo UNION_LOWER_HARDLINK_CREATE_OK
+busybox cat /hello-hard.txt | busybox grep "rootfs: module" && echo UNION_LOWER_HARDLINK_READ_OK
+busybox test -e /.upper/hello.txt && busybox test -e /.upper/hello-hard.txt && echo UNION_LOWER_HARDLINK_COPYUP_OK
+echo UNION_ROOT_BASE_PAYLOAD > /created.txt
+echo UNION_ROOT_APPEND_PAYLOAD >> /created.txt
+busybox cat /created.txt && echo UNION_ROOT_APPEND_CAT_OK
+busybox cat /.upper/created.txt && echo UNION_ROOT_UPPER_BACKING_OK
+UNION_LONG_NAME=bunix-union-root-name-longer-than-sixteen.txt
+echo UNION_ROOT_LONG_PAYLOAD > "/$UNION_LONG_NAME"
+busybox ls / | busybox grep "$UNION_LONG_NAME" && echo UNION_ROOT_LONG_READDIR_OK
+busybox cat "/$UNION_LONG_NAME" && echo UNION_ROOT_LONG_CAT_OK
+UNION_NAME_MAX=bunix-union-name-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.txt
+echo UNION_NAME_MAX_PAYLOAD > "/$UNION_NAME_MAX"
+busybox ls / | busybox grep "$UNION_NAME_MAX" && echo UNION_NAME_MAX_READDIR_OK
+busybox cat "/$UNION_NAME_MAX" && echo UNION_NAME_MAX_CAT_OK
+busybox ls / | busybox grep created.txt && echo UNION_ROOT_READDIR_UPPER_OK
+busybox ls / | busybox grep secret.txt && echo UNION_ROOT_READDIR_LOWER_OK
+busybox rm /created.txt && echo UNION_ROOT_UNLINK_UPPER_OK
+busybox test ! -e /created.txt && echo UNION_ROOT_UNLINK_UPPER_GONE_OK
+busybox cat /hello.txt && echo UNION_ROOT_LOWER_STILL_OK
+EOF_UNION_ROOT
 wait_for_fixed "$log" "UNION_ROOT_LOWER_STILL_OK" "unionfs root commands did not drain" 120 220
-printf 'busybox stat -c "%%u:%%g %%a" /tmp/bunix-write.txt\nbusybox mkdir /tmp/bunix-dir && echo TMP_MKDIR_OK\nbusybox test -d /tmp/bunix-dir && echo TMP_MKDIR_STAT_OK\nLONG_TMP_NAME=bunix-readdir-name-longer-than-sixteen.txt\necho TMP_LONG_READDIR_PAYLOAD > "/tmp/$LONG_TMP_NAME"\nbusybox ls /tmp | busybox grep "$LONG_TMP_NAME" && echo TMP_LONG_READDIR_OK\nbusybox cat "/tmp/$LONG_TMP_NAME" && echo TMP_LONG_NAME_CAT_OK\n' >&3
+send_script <<'EOF_TMPFS_BASELINE'
+busybox stat -c "%u:%g %a" /tmp/bunix-write.txt
+busybox mkdir /tmp/bunix-dir && echo TMP_MKDIR_OK
+busybox test -d /tmp/bunix-dir && echo TMP_MKDIR_STAT_OK
+LONG_TMP_NAME=bunix-readdir-name-longer-than-sixteen.txt
+echo TMP_LONG_READDIR_PAYLOAD > "/tmp/$LONG_TMP_NAME"
+busybox ls /tmp | busybox grep "$LONG_TMP_NAME" && echo TMP_LONG_READDIR_OK
+busybox cat "/tmp/$LONG_TMP_NAME" && echo TMP_LONG_NAME_CAT_OK
+EOF_TMPFS_BASELINE
 wait_for_fixed "$log" "TMP_LONG_NAME_CAT_OK" "tmpfs baseline long-name commands did not drain" 240 220
-printf 'TMP_NAME_MAX=bunix-tmp-name-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.txt\necho TMP_NAME_MAX_PAYLOAD > "/tmp/$TMP_NAME_MAX"\nbusybox ls /tmp | busybox grep "$TMP_NAME_MAX" && echo TMP_NAME_MAX_READDIR_OK\nbusybox cat "/tmp/$TMP_NAME_MAX" && echo TMP_NAME_MAX_CAT_OK\nbusybox touch /tmp/bunix-touch.txt && echo TMP_TOUCH_CREATE_OK\nbusybox touch /tmp/bunix-write.txt && echo TMP_TOUCH_EXISTING_OK\nbusybox mkfifo /tmp/bunix-fifo && echo TMP_MKFIFO_OK\nbusybox test -p /tmp/bunix-fifo && echo TMP_FIFO_STAT_OK\necho TMP_LINK_ONE > /tmp/bunix-link-src.txt\nbusybox ln /tmp/bunix-link-src.txt /tmp/bunix-link-dst.txt && echo TMP_HARDLINK_CREATE_OK\necho TMP_LINK_TWO >> /tmp/bunix-link-dst.txt\nbusybox cat /tmp/bunix-link-src.txt | busybox grep TMP_LINK_TWO && echo TMP_HARDLINK_SHARE_OK\nbusybox mv /tmp/bunix-link-src.txt /tmp/bunix-link-renamed.txt\necho TMP_LINK_THREE >> /tmp/bunix-link-renamed.txt\nbusybox cat /tmp/bunix-link-dst.txt | busybox grep TMP_LINK_THREE && echo TMP_HARDLINK_RENAME_OK\nbusybox mkdir /tmp/bunix-rmdir\nbusybox rmdir /tmp/bunix-rmdir\nbusybox test ! -e /tmp/bunix-rmdir && echo RMDIR_OK\nbusybox mkdir /tmp/bunix-rm-r-dir\nbusybox rm -r /tmp/bunix-rm-r-dir && echo RM_R_DIR_OK\nbusybox rm -rf /tmp/rename-src /tmp/rename-dst /tmp/rename-parent /tmp/rename-self\nbusybox mkdir /tmp/rename-src\nbusybox mkdir /tmp/rename-src/sub\necho TMP_DIR_RENAME_PAYLOAD > /tmp/rename-src/sub/file\nbusybox mv /tmp/rename-src /tmp/rename-dst && echo TMP_DIR_RENAME_NONEMPTY_OK\nbusybox cat /tmp/rename-dst/sub/file | busybox grep TMP_DIR_RENAME_PAYLOAD && echo TMP_DIR_RENAME_SUBTREE_OK\nbusybox test ! -e /tmp/rename-src && echo TMP_DIR_RENAME_OLD_GONE_OK\nbusybox mkdir /tmp/rename-parent\nbusybox mkdir /tmp/rename-parent/src\nbusybox mkdir /tmp/rename-parent/src/sub\nbusybox mkdir /tmp/rename-parent/empty\necho TMP_DIR_REPLACE_PAYLOAD > /tmp/rename-parent/src/sub/file\nbusybox mv -T /tmp/rename-parent/src /tmp/rename-parent/empty && echo TMP_DIR_REPLACE_EMPTY_OK\nbusybox cat /tmp/rename-parent/empty/sub/file | busybox grep TMP_DIR_REPLACE_PAYLOAD && echo TMP_DIR_REPLACE_SUBTREE_OK\nbusybox mkdir /tmp/rename-parent/src2\nbusybox mkdir /tmp/rename-parent/nonempty\necho target > /tmp/rename-parent/nonempty/file\nbusybox mv -f -T /tmp/rename-parent/src2 /tmp/rename-parent/nonempty || echo TMP_DIR_REPLACE_NONEMPTY_DENY_OK\nbusybox mkdir /tmp/rename-self\nbusybox mkdir /tmp/rename-self/child\nbusybox mv /tmp/rename-self /tmp/rename-self/child/moved || echo TMP_DIR_RENAME_DESCENDANT_DENY_OK\nbusybox test -d /tmp/rename-self/child && echo TMP_DIR_RENAME_DESCENDANT_PRESERVE_OK\nbusybox chmod 700 /tmp/bunix-write.txt && echo TMP_CHMOD_OK\nbusybox stat -c "%%a" /tmp/bunix-write.txt\nbusybox chown root:root /tmp/bunix-write.txt || echo TMP_CHOWN_DENY_OK\n' >&3
-printf 'NAME255=\ni=0\nwhile [ "$i" -lt 255 ]; do NAME255="${NAME255}a"; i=$((i + 1)); done\necho TMP_NAME255_PAYLOAD > "/tmp/$NAME255" && echo TMP_NAME255_CREATE_OK\nbusybox cat "/tmp/$NAME255" && echo TMP_NAME255_CAT_OK\nNAME256="${NAME255}b"\necho TMP_NAME256_PAYLOAD > "/tmp/$NAME256" || echo TMP_NAME256_DENY_OK\n' >&3
-printf 'echo TMP_SYMLINK_PAYLOAD > /tmp/bunix-symlink-target.txt\nbusybox ln -s bunix-symlink-target.txt /tmp/bunix-symlink.txt && echo TMP_SYMLINK_CREATE_OK\nbusybox readlink /tmp/bunix-symlink.txt | busybox grep bunix-symlink-target.txt && echo TMP_SYMLINK_READLINK_OK\nbusybox ls -l /tmp/bunix-symlink.txt | busybox grep bunix-symlink-target.txt && echo TMP_SYMLINK_LS_OK\n' >&3
-printf 'busybox ln -s hello.txt /union-symlink.txt && echo UNION_SYMLINK_CREATE_OK\nbusybox readlink /union-symlink.txt | busybox grep hello.txt && echo UNION_SYMLINK_READLINK_OK\nbusybox ls -l /union-symlink.txt | busybox grep hello.txt && echo UNION_SYMLINK_LS_OK\necho UNION_HARDLINK_ONE > /union-hard-src.txt\nbusybox ln /union-hard-src.txt /union-hard-dst.txt && echo UNION_HARDLINK_CREATE_OK\necho UNION_HARDLINK_TWO >> /union-hard-dst.txt\nbusybox cat /union-hard-src.txt | busybox grep UNION_HARDLINK_TWO && echo UNION_HARDLINK_SHARE_OK\n' >&3
+send_script <<'EOF_TMPFS_EXTENDED'
+TMP_NAME_MAX=bunix-tmp-name-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.txt
+echo TMP_NAME_MAX_PAYLOAD > "/tmp/$TMP_NAME_MAX"
+busybox ls /tmp | busybox grep "$TMP_NAME_MAX" && echo TMP_NAME_MAX_READDIR_OK
+busybox cat "/tmp/$TMP_NAME_MAX" && echo TMP_NAME_MAX_CAT_OK
+busybox touch /tmp/bunix-touch.txt && echo TMP_TOUCH_CREATE_OK
+busybox touch /tmp/bunix-write.txt && echo TMP_TOUCH_EXISTING_OK
+busybox mkfifo /tmp/bunix-fifo && echo TMP_MKFIFO_OK
+busybox test -p /tmp/bunix-fifo && echo TMP_FIFO_STAT_OK
+echo TMP_LINK_ONE > /tmp/bunix-link-src.txt
+busybox ln /tmp/bunix-link-src.txt /tmp/bunix-link-dst.txt && echo TMP_HARDLINK_CREATE_OK
+echo TMP_LINK_TWO >> /tmp/bunix-link-dst.txt
+busybox cat /tmp/bunix-link-src.txt | busybox grep TMP_LINK_TWO && echo TMP_HARDLINK_SHARE_OK
+busybox mv /tmp/bunix-link-src.txt /tmp/bunix-link-renamed.txt
+echo TMP_LINK_THREE >> /tmp/bunix-link-renamed.txt
+busybox cat /tmp/bunix-link-dst.txt | busybox grep TMP_LINK_THREE && echo TMP_HARDLINK_RENAME_OK
+busybox mkdir /tmp/bunix-rmdir
+busybox rmdir /tmp/bunix-rmdir
+busybox test ! -e /tmp/bunix-rmdir && echo RMDIR_OK
+busybox mkdir /tmp/bunix-rm-r-dir
+busybox rm -r /tmp/bunix-rm-r-dir && echo RM_R_DIR_OK
+busybox rm -rf /tmp/rename-src /tmp/rename-dst /tmp/rename-parent /tmp/rename-self
+busybox mkdir /tmp/rename-src
+busybox mkdir /tmp/rename-src/sub
+echo TMP_DIR_RENAME_PAYLOAD > /tmp/rename-src/sub/file
+busybox mv /tmp/rename-src /tmp/rename-dst && echo TMP_DIR_RENAME_NONEMPTY_OK
+busybox cat /tmp/rename-dst/sub/file | busybox grep TMP_DIR_RENAME_PAYLOAD && echo TMP_DIR_RENAME_SUBTREE_OK
+busybox test ! -e /tmp/rename-src && echo TMP_DIR_RENAME_OLD_GONE_OK
+busybox mkdir /tmp/rename-parent
+busybox mkdir /tmp/rename-parent/src
+busybox mkdir /tmp/rename-parent/src/sub
+busybox mkdir /tmp/rename-parent/empty
+echo TMP_DIR_REPLACE_PAYLOAD > /tmp/rename-parent/src/sub/file
+busybox mv -T /tmp/rename-parent/src /tmp/rename-parent/empty && echo TMP_DIR_REPLACE_EMPTY_OK
+busybox cat /tmp/rename-parent/empty/sub/file | busybox grep TMP_DIR_REPLACE_PAYLOAD && echo TMP_DIR_REPLACE_SUBTREE_OK
+busybox mkdir /tmp/rename-parent/src2
+busybox mkdir /tmp/rename-parent/nonempty
+echo target > /tmp/rename-parent/nonempty/file
+busybox mv -f -T /tmp/rename-parent/src2 /tmp/rename-parent/nonempty || echo TMP_DIR_REPLACE_NONEMPTY_DENY_OK
+busybox mkdir /tmp/rename-self
+busybox mkdir /tmp/rename-self/child
+busybox mv /tmp/rename-self /tmp/rename-self/child/moved || echo TMP_DIR_RENAME_DESCENDANT_DENY_OK
+busybox test -d /tmp/rename-self/child && echo TMP_DIR_RENAME_DESCENDANT_PRESERVE_OK
+busybox chmod 700 /tmp/bunix-write.txt && echo TMP_CHMOD_OK
+busybox stat -c "%a" /tmp/bunix-write.txt
+busybox chown root:root /tmp/bunix-write.txt || echo TMP_CHOWN_DENY_OK
+NAME255=
+i=0
+while [ "$i" -lt 255 ]; do NAME255="${NAME255}a"; i=$((i + 1)); done
+echo TMP_NAME255_PAYLOAD > "/tmp/$NAME255" && echo TMP_NAME255_CREATE_OK
+busybox cat "/tmp/$NAME255" && echo TMP_NAME255_CAT_OK
+NAME256="${NAME255}b"
+echo TMP_NAME256_PAYLOAD > "/tmp/$NAME256" || echo TMP_NAME256_DENY_OK
+echo TMP_SYMLINK_PAYLOAD > /tmp/bunix-symlink-target.txt
+busybox ln -s bunix-symlink-target.txt /tmp/bunix-symlink.txt && echo TMP_SYMLINK_CREATE_OK
+busybox readlink /tmp/bunix-symlink.txt | busybox grep bunix-symlink-target.txt && echo TMP_SYMLINK_READLINK_OK
+busybox ls -l /tmp/bunix-symlink.txt | busybox grep bunix-symlink-target.txt && echo TMP_SYMLINK_LS_OK
+busybox ln -s hello.txt /union-symlink.txt && echo UNION_SYMLINK_CREATE_OK
+busybox readlink /union-symlink.txt | busybox grep hello.txt && echo UNION_SYMLINK_READLINK_OK
+busybox ls -l /union-symlink.txt | busybox grep hello.txt && echo UNION_SYMLINK_LS_OK
+echo UNION_HARDLINK_ONE > /union-hard-src.txt
+busybox ln /union-hard-src.txt /union-hard-dst.txt && echo UNION_HARDLINK_CREATE_OK
+echo UNION_HARDLINK_TWO >> /union-hard-dst.txt
+busybox cat /union-hard-src.txt | busybox grep UNION_HARDLINK_TWO && echo UNION_HARDLINK_SHARE_OK
+EOF_TMPFS_EXTENDED
 wait_for_fixed "$log" "TMP_CHOWN_DENY_OK" "tmpfs extended long-name commands did not drain" 45 220
-printf 'busybox head -c 8192 /dev/zero > /tmp/linux-big-write.bin\nbusybox test "$(busybox stat -c "%%s" /tmp/linux-big-write.bin)" = "8192" && echo LINUX_BIG_WRITE_OK\n' >&3
-printf '/bin/mmapbig && echo MMAPBIG_OK\n/bin/mmaphuge\n/bin/readbig && echo READBIG_OK\n' >&3
+send_script <<'EOF_LARGE_IO'
+busybox head -c 8192 /dev/zero > /tmp/linux-big-write.bin
+busybox test "$(busybox stat -c "%s" /tmp/linux-big-write.bin)" = "8192" && echo LINUX_BIG_WRITE_OK
+/bin/mmapbig && echo MMAPBIG_OK
+/bin/mmaphuge
+/bin/readbig && echo READBIG_OK
+EOF_LARGE_IO
 wait_for_fixed "$log" "READBIG_OK" "pre-mount shell commands did not drain" 45 220
-printf 'mount -t tmpfs tmpfs /mnt&&echo MNT_OK\necho MNT_PAYLOAD>/mnt/linux-mount.txt\ncat /mnt/linux-mount.txt&&echo MNT_CAT\nmount|busybox grep /mnt&&echo MNT_LIST\numount /mnt&&echo UMNT_OK\nmount|busybox grep /mnt||echo UMNT_GONE\ntest ! -e /mnt/linux-mount.txt&&echo UMNT_HIDE\n' >&3
+send_script <<'EOF_MOUNT_TMPFS'
+mount -t tmpfs tmpfs /mnt&&echo MNT_OK
+echo MNT_PAYLOAD>/mnt/linux-mount.txt
+cat /mnt/linux-mount.txt&&echo MNT_CAT
+mount|busybox grep /mnt&&echo MNT_LIST
+umount /mnt&&echo UMNT_OK
+mount|busybox grep /mnt||echo UMNT_GONE
+test ! -e /mnt/linux-mount.txt&&echo UMNT_HIDE
+EOF_MOUNT_TMPFS
 
 wait_for_fixed "$log" "load average" "busybox uptime did not run through applet argv" 45 120
 wait_for_fixed "$log" " 1 users" "busybox uptime did not report login session" 45 160
